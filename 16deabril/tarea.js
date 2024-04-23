b8b093ff38abdae88017b8633b753f9fed038b81
AndresSg0716 <jandressg30@gmail.com>
2024-04-23 15:27:08 -0500

    x

--- 16deabril/tarea.js
+++ 16deabril/tarea.js
@@ -41,10 +41,10 @@ function factoryUsuario(nombreU, contraseña, email, rolU, pedido, pedido2,) {
     }
 }
 
-let orden1 = factoryUsuario("Andres", "312312", "jandressg30@gmail.com", factoryRol(1, "cliente"),factoryPedido(1, "12/12/12" , "Envigado", factoryProducto(1, "Play Station 5", 500), factoryProducto(2, "Xbox Series X", 400)), factoryPedido(2, "10/10/10", "Envigado", factoryProducto(3, "Licuadora", 50),factoryProducto(4,"lavadora", 75)))
+let orden = factoryUsuario("Andres", "312312", "jandressg30@gmail.com", factoryRol(1, "admin"),factoryPedido(1, "12/12/12" , "Envigado", factoryProducto(1, "Play Station 5", 500), factoryProducto(2, "Xbox Series X", 400)), factoryPedido(2, "10/10/10", "Envigado", factoryProducto(3, "Licuadora", 50),factoryProducto(4,"lavadora", 75)))
 
 let orden2 = factoryUsuario("Julian", "123412", "julianperez@gmail.com", factoryRol(1, "cliente"),factoryPedido(3, "12/09/23" , "Itagui", factoryProducto(5, "Televisor Samsung", 700), factoryProducto(2, "Xbox Series X", 400)), factoryPedido(4, "11/09/23", "Envigado", factoryProducto(7, "Audifonos Bluethot", 60),factoryProducto(4,"lavadora", 75)))
 
 
-console.log(orden1);
-console.log(orden2);
+console.log(orden.rolU);
+
